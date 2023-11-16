cb0c38498f1feb1e056694606225c3b4c0ffbab7
Avitus <wcbderoxtam@gmail.com>
2023-11-16 18:13:01 +0300

    deploy

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+
+<i class="fas fa-star" onmouseover="highlightIcon(this)" onmouseout="removeHighlight(this)"></i>
+
+script>
+    function highlightIcon(icon) {
+        icon.style.color = 'gold';
+    }
+
+    function removeHighlight(icon) {
+        icon.style.color = ''; 
+    }
+</script>
